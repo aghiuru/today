c502e2dc72987dc311d37abfb5ac4a53be5ccae5
Andrei Ghiuru <aghiuru@users.noreply.github.com>
2018-04-16 20:16:43 +0200

    Initial Commit

---
+++ today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  today
+//
+//  Created by Andrei Ghiuru on 4/16/18.
+//  Copyright © 2018 Andrei Ghiuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
